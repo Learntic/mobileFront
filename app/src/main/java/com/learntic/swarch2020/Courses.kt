3d360959aaa65f12e5298af72ffee01b7313f9aa
camilonfs1 <camilonfs1@gmail.com>
2020-10-13 21:18:49 -0500

    primeras vistas

---
+++ app/src/main/java/com/learntic/swarch2020/Courses.kt
@@ -0,0 +1,11 @@
+package com.learntic.swarch2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Courses : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_courses)
+    }
+}
---
+++ app/src/main/java/com/learntic/swarch2020/Specific_Course.kt
@@ -0,0 +1,11 @@
+package com.learntic.swarch2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Specific_Course : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_specific__course)
+    }
+}
